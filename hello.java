5cff91dee637002316a9c4b63164cf2e90276665
manda ayzsy <mandaayzsy@gmail.com>
2023-09-21 12:37:21 +0700

    first commit

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class hello{
+    public static void main(String[] args){
+        String name[] = {"Manda","D3IF03","03"};
+        System.out.println(name[0]);
+        System.out.println(name[1]);
+        System.out.println(name[2]);
+    }
+}
